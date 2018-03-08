140f312b768392235fe4eb994bec2921cb06cf5e
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-03-07 21:47:27 -0300

    opcao 6 adicionada ao menu

--- biblioteca.pl
+++ biblioteca.pl
@@ -1,108 +1,120 @@
-﻿carregaArquivo:-
-	consult('./data/livros.pl'). 
+carregaArquivo:-
+    consult('./data/livros.pl'). 
 
 adicionaLivro(Titulo,Autor,Editora,Estado):-
-	asserta(livro(Titulo,Autor,Editora,Estado)).
+    asserta(livro(Titulo,Autor,Editora,Estado)).
 
 adicionaAlugado(Titulo):-
-	livro(Titulo,Autor,Editora,D), 
-	D == disponivel,
-	adicionaLivro(Titulo,Autor,Editora,alugado),
-	excluiLivro(Titulo,disponivel), !.
+    livro(Titulo,Autor,Editora,D), 
+    D == disponivel,
+    adicionaLivro(Titulo,Autor,Editora,alugado),
+    excluiLivro(Titulo,disponivel), !.
 
 devolveLivro(Titulo):-
-	livro(Titulo,Autor,Editora,D),
-	D == alugado,
-	adicionaLivro(Titulo,Autor,Editora,disponivel),
-	excluiLivro(Titulo,alugado).
-	
-excluiLivro(Titulo,Estado):-
-	retract(livro(Titulo,_,_,Estado)).
+    livro(Titulo,Autor,Editora,D),
+    D == alugado,
+    adicionaLivro(Titulo,Autor,Editora,disponivel),
+    excluiLivro(Titulo,alugado).
+    
+excluiLivro(Titulo, Estado):-
+    retract(livro(Titulo,_,_,Estado)),!.
+
+
 
 listaLivrosDisponiveis:-
-	livro(Titulo,Autor,Editora,disponivel),
-	format('Livro = ~w', [Titulo]),
-	format('  Autor = ~w', [Autor]),
-	format('  Editora = ~w', [Editora]),
-	nl,nl.
+    livro(Titulo,Autor,Editora,disponivel),
+    format('Livro = ~w', [Titulo]),
+    format('  Autor = ~w', [Autor]),
+    format('  Editora = ~w', [Editora]),
+    nl,nl.
 
 
 listaLivrosAlugados:-
-	livro(Titulo,Autor,Editora,alugado),
-	format('Livro = ~w', [Titulo]),
-	format('  Autor = ~w', [Autor]),
-	format('  Editora = ~w', [Editora]),
-	nl,nl.
+    livro(Titulo,Autor,Editora,alugado),
+    format('Livro = ~w', [Titulo]),
+    format('  Autor = ~w', [Autor]),
+    format('  Editora = ~w', [Editora]),
+    nl,nl.
 
 salva:-
-	tell('./data/livros.pl'),
-	listing(livro),
-	told.
+    tell('./data/livros.pl'),
+    listing(livro),
+    told.
 
 acao(0):-
-	writeln("Programa finalizado"),
-	salva.
+    writeln("Programa finalizado"),
+    salva, nl.
 
 acao(1):-
-	write('Digite o nome do livro: '),
-	read(Titulo),
-	write('Digite o autor do livro: '),
-	read(Autor),
-	write('Digite a editora do livro: '),
-	read(Editora),
-	adicionaLivro(Titulo,Autor,Editora,disponivel),
-	salva,
-	nl,
-	writeln('Livro adicionado com sucesso'),
-	nl, menu.
+    write('Digite o nome do livro: '),
+    read(Titulo),
+    write('Digite o autor do livro: '),
+    read(Autor),
+    write('Digite a editora do livro: '),
+    read(Editora),
+    adicionaLivro(Titulo,Autor,Editora,disponivel),
+    salva,
+    nl,
+    writeln('Livro adicionado com sucesso'),
+    nl, menu.
 
 acao(2):-
-	writeln('Digite o nome do livro.'),
-	read(Titulo),
-	adicionaAlugado(Titulo),
-	salva,
-	writeln('Livro alugado com sucesso'),
-	nl, menu.
+    writeln('Digite o nome do livro.'),
+    read(Titulo),
+    adicionaAlugado(Titulo),
+    salva,
+    writeln('Livro alugado com sucesso'),
+    nl, menu.
 
 acao(3):-
-	writeln('Digite o nome do livro.'),
-	read(Titulo),
-	devolveLivro(Titulo),
-	salva,
-	writeln('Livro devolvido com sucesso'),
-	nl, menu.
+    writeln('Digite o nome do livro.'),
+    read(Titulo),
+    devolveLivro(Titulo),
+    salva,
+    writeln('Livro devolvido com sucesso'),
+    nl, menu.
 
 acao(4):-
-	listaLivrosDisponiveis,
-	menu.
+    listaLivrosDisponiveis,
+    menu.
 
 acao(5):-
-	listaLivrosAlugados,
-	menu.
+    listaLivrosAlugados,
+    menu.
+
+acao(6):-
+    writeln('Digite o nome do livro.'),
+    read(Titulo),
+    writeln('Digite o status do livro.("disponivel", "alugado")'),
+    read(Estado),
+    excluiLivro(Titulo, Estado),
+    nl, menu.
 
 acao(X):-
-	writeln("Opção inválida, tente novamente."),
-	menu.
+    writeln("Opção inválida, tente novamente."),
+    menu.
 
 menu:-
-	
-	nl,
-	writeln('Bem-Vindo.'),
-	writeln('Menu de opcoes:'),
-	writeln('1-Adicionar livro a biblioteca'),
-	writeln('2-Alugar livro'),
-	writeln('3-Devolve Livro'),
-	writeln('4-Lista livros disponiveis'),
-	writeln('5-Lista livros alugados'),
-	writeln('Digite 0 para sair'),
-	read(Resposta),nl,
-	acao(Resposta).
-	
+    
+    nl,
+    writeln('Bem-Vindo.'),
+    writeln('Menu de opcoes:'),
+    writeln('1-Adicionar livro a biblioteca'),
+    writeln('2-Alugar livro'),
+    writeln('3-Devolve Livro'),
+    writeln('4-Lista livros disponiveis'),
+    writeln('5-Lista livros alugados'),
+    writeln('6-Remover livro da biblioteca'),
+    writeln('Digite 0 para sair'),
+    read(Resposta),nl,
+    acao(Resposta).
+    
 start:-
-	carregaArquivo,
-	menu.
-	
+    carregaArquivo,
+    menu.
+    
 :-initialization main.
 main:-
-	
-	start.
+    
+    start.
+
